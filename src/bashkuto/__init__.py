b5a9d0124c92d4a33de2dc17935b64a458bc8fb2
Mohamed Sofiene Kadri <ms.kadri.dev@gmail.com>
2026-03-17 01:38:49 +0100

    add: api upgrades

--- src/bashkuto/__init__.py
+++ src/bashkuto/__init__.py
@@ -1,9 +1,27 @@
 from .api import run
 from .runtime.runtime import BashRuntime
 from .runtime.result import CommandResult
+from .runtime.exceptions import (
+    BashkutoError,
+    SecurityError,
+    TimeoutError,
+    BinaryOutputError,
+    OverflowError,
+)
+from .presentation.formatter import OutputFormatter, format_result
 
 __all__ = [
+    # Main API
     "run",
     "BashRuntime",
-    "CommandResult"
+    "CommandResult",
+    # Exceptions
+    "BashkutoError",
+    "SecurityError",
+    "TimeoutError",
+    "BinaryOutputError",
+    "OverflowError",
+    # Formatting
+    "OutputFormatter",
+    "format_result",
 ]
